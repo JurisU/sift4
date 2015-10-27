c63c293e8a2904263d265d02f40821613977d6b3
Jason Hutchinson <Jason.Hutchinson@samsclub.com>
2015-10-27 22:28:33 +0000

    add getters and setters

--- main.go
+++ main.go
@@ -55,3 +55,53 @@ func defaultTranspositionCostEvaluator(c1, c2 int) int {
 func defaultTranspositionsEvaluator(lcss, trans int) int {
 	return lcss - trans
 }
+
+// Getters & Setters
+
+func (s *Sift4) SetTokenizer(fn func(string) []string) *Sift4 {
+	s.tokenizer = fn
+	return s
+}
+func (s *Sift4) GetTokenizer() func(string) []string {
+	return s.tokenizer
+}
+
+func (s *Sift4) SetTokenMatcher(fn func(string, string) bool) *Sift4 {
+	s.tokenMatcher = s
+	return s
+}
+func (s *Sift4) GetTokenMatcher() func(string, string) bool {
+	return s.tokenMatcher
+}
+
+func (s *Sift4) SetMatchingEvaluator(fn func(string, string) int) *Sift4 {
+	s.matchingEvaluator = fn
+	return s
+}
+func (s *Sift4) GetMatchingEvaluator() func(string, string) int {
+	return s.matchingEvaluator
+}
+
+func (s *Sift4) SetLocalLengthEvaluator(fn func(int) int) *Sift4 {
+	s.localLengthEvaluator = fn
+	return s
+}
+func (s *Sift4) GetLocalLengthEvaluator() func(int) int {
+	return s.localLengthEvaluator
+}
+
+func (s *Sift4) SetTranspositionCostEvaluator(fn func(string, string) int) *Sift4 {
+	s.transpositionCostEvaluator = fn
+	return s
+}
+func (s *Sift4) GetTranspositionCostEvaluator() func(string, string) int {
+	return s.transpositionCostEvaluator
+}
+
+func (s *Sift4) SetTranspositionsEvaluator(fn func(int, int) int) *Sift4 {
+	s.transpositionsEvaluator = fn
+	return s
+}
+func (s *Sift4) GetTranspositionsEvaluator() func(int, int) int {
+	return s.transpositionsEvaluator
+}
