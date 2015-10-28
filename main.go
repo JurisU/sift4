7be2b4f61a735aa45de66b8f4785e017457f7d22
Jason Hutchinson <Jason.Hutchinson@samsclub.com>
2015-10-28 14:33:47 +0000

    s/int/float64/ + maxOffset

--- main.go
+++ main.go
@@ -7,19 +7,21 @@ import (
 // Models
 
 type Sift4 struct {
+	maxOffset                  int
 	maxDistance                int
 	tokenizer                  func(string) []string
 	tokenMatcher               func(string, string) bool
-	matchingEvaluator          func(string, string) int
-	localLengthEvaluator       func(int) int
-	transpositionCostEvaluator func(string, string) int
-	transpositionsEvaluator    func(int, int) int
+	matchingEvaluator          func(string, string) float64
+	localLengthEvaluator       func(float64) float64
+	transpositionCostEvaluator func(int, int) float64
+	transpositionsEvaluator    func(float64, float64) float64
 }
 
 // Initialization
 
 func New() *Sift4 {
 	return &Sift4{
+		maxOffset:                  0,
 		maxDistance:                0,
 		tokenizer:                  defaultTokenizer,
 		tokenMatcher:               defaultTokenMatcher,
@@ -40,23 +42,30 @@ func defaultTokenMatcher(t1, t2 string) bool {
 	return t1 == t2
 }
 
-func defaultMatchingEvaluator(t1, t2 string) int {
-	return 1
+func defaultMatchingEvaluator(t1, t2 string) float64 {
+	return 1.0
 }
 
-func defaultLocalLengthEvaluator(i int) int {
+func defaultLocalLengthEvaluator(i float64) float64 {
 	return i
 }
 
-func defaultTranspositionCostEvaluator(c1, c2 int) int {
-	return 1
+func defaultTranspositionCostEvaluator(c1, c2 int) float64 {
+	return 1.0
 }
 
-func defaultTranspositionsEvaluator(lcss, trans int) int {
+func defaultTranspositionsEvaluator(lcss, trans float64) float64 {
 	return lcss - trans
 }
 
 // Getters & Setters
+func (s *Sift4) SetMaxOffset(i int) *Sift4 {
+	s.maxOffset = i
+	return s
+}
+func (s *Sift4) GetMaxOffset() int {
+	return s.maxOffset
+}
 
 func (s *Sift4) SetTokenizer(fn func(string) []string) *Sift4 {
 	s.tokenizer = fn
@@ -67,41 +76,41 @@ func (s *Sift4) GetTokenizer() func(string) []string {
 }
 
 func (s *Sift4) SetTokenMatcher(fn func(string, string) bool) *Sift4 {
-	s.tokenMatcher = s
+	s.tokenMatcher = fn
 	return s
 }
 func (s *Sift4) GetTokenMatcher() func(string, string) bool {
 	return s.tokenMatcher
 }
 
-func (s *Sift4) SetMatchingEvaluator(fn func(string, string) int) *Sift4 {
+func (s *Sift4) SetMatchingEvaluator(fn func(string, string) float64) *Sift4 {
 	s.matchingEvaluator = fn
 	return s
 }
-func (s *Sift4) GetMatchingEvaluator() func(string, string) int {
+func (s *Sift4) GetMatchingEvaluator() func(string, string) float64 {
 	return s.matchingEvaluator
 }
 
-func (s *Sift4) SetLocalLengthEvaluator(fn func(int) int) *Sift4 {
+func (s *Sift4) SetLocalLengthEvaluator(fn func(float64) float64) *Sift4 {
 	s.localLengthEvaluator = fn
 	return s
 }
-func (s *Sift4) GetLocalLengthEvaluator() func(int) int {
+func (s *Sift4) GetLocalLengthEvaluator() func(float64) float64 {
 	return s.localLengthEvaluator
 }
 
-func (s *Sift4) SetTranspositionCostEvaluator(fn func(string, string) int) *Sift4 {
+func (s *Sift4) SetTranspositionCostEvaluator(fn func(int, int) float64) *Sift4 {
 	s.transpositionCostEvaluator = fn
 	return s
 }
-func (s *Sift4) GetTranspositionCostEvaluator() func(string, string) int {
+func (s *Sift4) GetTranspositionCostEvaluator() func(int, int) float64 {
 	return s.transpositionCostEvaluator
 }
 
-func (s *Sift4) SetTranspositionsEvaluator(fn func(int, int) int) *Sift4 {
+func (s *Sift4) SetTranspositionsEvaluator(fn func(float64, float64) float64) *Sift4 {
 	s.transpositionsEvaluator = fn
 	return s
 }
-func (s *Sift4) GetTranspositionsEvaluator() func(int, int) int {
+func (s *Sift4) GetTranspositionsEvaluator() func(float64, float64) float64 {
 	return s.transpositionsEvaluator
 }
